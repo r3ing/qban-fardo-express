9a89f3265284c2e38a2665f42ceb2763606f317a
CFEXP <reirpinf@gmail.com>
2025-01-21 10:22:55 -0500

    init

---
+++ src/test/java/com/reing/QbanFardoExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbanFardoExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
